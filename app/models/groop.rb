4afd2e32c5ce3d7622e0fc4da5aa808dd2ebf433
Kirill Agatov <abardacha@gmail.com>
2015-10-03 01:20:09 +0300

    доработка

--- app/models/groop.rb
+++ app/models/groop.rb
@@ -2,6 +2,9 @@ class Groop < ActiveRecord::Base
   belongs_to :workshop
   belongs_to :place
 
+  validates :workshop_id, presence: true
+  validates :place_id, presence: true
+
   def seats_available
     if total_seats and reserved_seats
       total_seats - reserved_seats 
