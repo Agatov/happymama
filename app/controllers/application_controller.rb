ee65be33779b5be508e3f7b9a5b6892ab41238a9
Kirill Agatov <abardacha@gmail.com>
2016-03-06 18:23:39 +0300

    fx

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  after_action :allow_iframe
 
   def index
     @categories = Category.order(:id)
@@ -25,5 +26,9 @@ class ApplicationController < ActionController::Base
 
     @reviews_count = Review.count
     @discount_workshop = Workshop.where('discount is not null').first
+  end
+
+  def allow_iframe
+    response.headers.delete "X-Frame-Options"
   end
 end
